f298ef4e653a6c58f46b862b55fc490edce4a798
Lucas Alves <lucas@kissolutions.tech>
2018-10-01 10:35:17 -0300

    Adding base for Trabalho 1

---
+++ filtragem-linear/codigo/cmake-build-debug/CMakeFiles/BUILD.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BUILD"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/BUILD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
